86e519b69a8fade0a43a11c6dbda5a3b9ae8d3a2
Trevar Williams <williams.trevar@gmail.com>
2019-02-01 22:17:44 -0800

    Updated js files for database support

--- server.js
+++ server.js
@@ -1,15 +1,102 @@
+'use strict';
+
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const blogPostsRouter = require("./blogPostRouter");
+mongoose.Promise = global.Promise;
+
 const { PORT, DATABASE_URL } = require("./config");
+const { BlogPost } = require('./models');
 
 const app = express();
 
 app.use(morgan("common")); 
 app.use(express.json());
-app.use("/blog-posts", blogPostsRouter);
+
+// GET
+app.get('/posts', (req, res) => {
+  BlogPost.find()
+    .then(posts => {
+      res.json(posts.map(post => post.serialize()));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+// GET by id
+app.get('/posts/:id', (req, res) => {
+  BlogPost
+    .findById(req.params.id)
+    .then(post => res.json(post.serialize))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  });
+});
+
+// POST
+app.post('/posts', (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  BlogPost
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author
+    })
+    .then(blogPost => res.status(201).json(blogPost.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+// PUT
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message =
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
+  
+  const toUpdate = {};
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      update[field] = req.body[field];
+    }
+  });
+
+  BlogPost
+    .findByIdAndUpdate(req.params.id, { $set: updated })
+    .then(BlogPost => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+// DELETE
+app.delete('/posts/:id', (req, res) => {
+  BlogPost.findByIdAndRemove(req.params.id)
+    .then(BlogPost => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+// Non-existent endpoint
+app.use('*', function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
+});
 
 let server;
 
